78978d3f1327045d8a371f43e057bfd5dbb296ed
Tdavide04 <tdavide833@gmail.com>
2024-03-01 10:53:15 +0100

    svolto l'esercizio e aggiustato

--- esercizio 7.py
+++ esercizio 7.py
@@ -4,9 +4,11 @@
 #costruire una terza lista che contiene prima tutti i pari e poi tutti i dispari
 
 lista_1 = []
-for n in range(2, 1000, 2):
+for n in range(2, 1001, 2):
     lista_1.append(n)
 lista_2 = []
 for n in range(1, 1000, 2):
-    lista_1.append(n)
+    lista_2.append(n)
 
+lista_3 = lista_1 + lista_2
+print(lista_3)
