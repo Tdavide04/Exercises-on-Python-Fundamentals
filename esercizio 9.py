17a8702674151d87065b4c418038525f2d5d8eb1
Tdavide04 <tdavide833@gmail.com>
2024-03-07 18:50:26 +0100

    risolto problema all'esercizio del prof

--- esercizio 9.py
+++ esercizio 9.py
@@ -1,16 +1,14 @@
-numero = ""
+numero = "1"
 while True:
     c = input("Digita 0-9,+-/=: ")
     if len(c) > 0:
         c = c[0]
-    # dovete leggere un numero sia intero, sia decimale
-    # e stamparlo nella sua interezza quando viene
-    # digitato un simbolo non numerico (+-/=)
-    #
-    # Terminerete quando varrà la
+
     if c not in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ","]:
-        # Stampate il numero letto
         print("Il numero è: ", numero)
+        
+    if c == "," and numero.count(",")>=1:
+        print("Errore")
         break
     else:
         # costruzione del numero...
