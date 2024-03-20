93a7068a85b5bedb96fbe3692614ab942c06529a
Tdavide04 <tdavide833@gmail.com>
2024-03-20 16:12:55 +0100

    me so scassato

---
+++ compito 1.py
@@ -0,0 +1,103 @@
+import json
+
+#Per leggere un file json 
+filejson = open("all-world-cup-players.json", "r")
+worldcup = json.load(filejson)
+filejson.close()
+'''
+print(len(worldcup))
+
+print(worldcup[1200])
+print(worldcup[1200]['DateOfBirth'])
+'''
+
+# !!!! SONO TUTTE OPERAZIONI SU LISTE !!!!
+# 1) contare quanti calciatori hanno giocato per l'Italia
+# 2) contare quanti calciatori hanno giocato per il Brasile
+# 3) contare quanti calciatori hanno giocato per l'Argentina
+# 4) Indicare quali calciatori hanno giocato sia per il Brasile, sia per l'Italia
+# 5) Indicare quali calciatori hanno giocato sia per l'Argentina, sia per l'Italia
+# 6) Trovare qual'è il calciatore più giovane che ha partecipato alla coppa del mondo
+# 7) Trovare qual'è il calciatore più anziano che ha partecipato alla coppa del mondo
+# 8) Trovare quale calciatore ha partecipato a più edizioni della coppa del mondo
+# 9) Trovare quale squadra di calcio ha fornito più calciatori per la coppa del mondo
+#    Organizzare per nazione. Cioè quale squadra italiana ha fornito più calciatori per la coppa del mondo e quanti, quale squadra francese, ...
+
+
+'''
+quantiCalciatori = dict()
+for v in worldcup:
+    # se v.Team è già presente, sommo 1, altrimenti metto a 1
+    if v["Team"] in quantiCalciatori.keys():
+        quantiCalciatori[v["Team"]]=quantiCalciatori[v["Team"]]+1
+    else:
+        quantiCalciatori[v["Team"]]=1
+        
+Italia = []
+for x in quantiCalciatori:
+    if "Italy" in x:
+       Italia.append(x)
+
+print(len(Italia))
+
+'''
+
+# 1) contare quanti calciatori hanno giocato per l'Italia
+giocatoriItaliani = 0
+for player in worldcup:
+    if player["ClubCountry"] == "Italy":
+        giocatoriItaliani +=1
+print("il numeri di giocatori Italiani è: ", giocatoriItaliani)
+
+# 2) contare quanti calciatori hanno giocato per il Brasile
+giocatoriBrasiliani = 0
+for player in worldcup:
+    if player["ClubCountry"] == "Brazil":
+        giocatoriBrasiliani +=1
+print("il numeri di giocatori Brasiliani è: ", giocatoriBrasiliani)
+
+# 3) contare quanti calciatori hanno giocato per l'Argentina
+giocatoriArgentini = 0
+for player in worldcup:
+    if player["ClubCountry"] == "Argentina":
+        giocatoriArgentini +=1
+print("il numeri di giocatori Argentini è: ", giocatoriArgentini)
+
+# 4) Indicare quali calciatori hanno giocato sia per il Brasile, sia per l'Italia
+giocatori_Italia_Brasile = 0
+for player in worldcup:
+    if player["ClubCountry"] == "Italia" and player["ClubCountry"] == "Brazil":
+        giocatori_Italia_Brasile +=1
+print("il numeri di giocatori che hanno giocato sia in Italia che in Brasile è: ", giocatori_Italia_Brasile)
+
+# 5) Indicare quali calciatori hanno giocato sia per l'Argentina, sia per l'Italia
+giocatori_Italia_Argentina = 0
+for player in worldcup:
+    if player["ClubCountry"] == "Italia" and player["ClubCountry"] == "Argentina":
+        giocatori_Italia_Argentina +=1
+print("il numeri di giocatori che hanno giocato sia in Italia che in Argentina è: ", giocatori_Italia_Argentina)
+
+# 6) Trovare qual'è il calciatore più giovane che ha partecipato alla coppa del mondo
+giocatore_piu_giovane = {}
+for player in worldcup:
+    Cup = player.get("Year")
+    Year = player.get("DateOfBirth")
+    year = int(Year[0:4])
+print(year)
+# 7) Trovare qual'è il calciatore più anziano che ha partecipato alla coppa del mondo
+
+# 8) Trovare quale calciatore ha partecipato a più edizioni della coppa del mondo
+partecipazioni_giocatori = {}
+for player in worldcup:
+    full_name = player.get('FullName')
+    if full_name:
+        partecipazioni_giocatori[full_name] = partecipazioni_giocatori.get(full_name, 0) + 1
+giocatore_piu_frequente = max(partecipazioni_giocatori, key=partecipazioni_giocatori.get)
+
+print("Il giocatore che ha partecipato al maggior numero di edizioni della Coppa del Mondo fino a questo punto è: ", giocatore_piu_frequente)
+
+# 9) Trovare quale squadra di calcio ha fornito più calciatori per la coppa del mondo
+#    Organizzare per nazione. Cioè quale squadra italiana ha fornito più calciatori per la coppa del mondo e quanti, quale squadra francese, ...
+
+
+
